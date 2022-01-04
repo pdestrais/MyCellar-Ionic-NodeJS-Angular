60c66a15f3cfbae678752255506fa30651b7d472
Destrais <philippe.destrais@gmail.com>
2022-01-04 14:30:43 +0100

    chnages for GH issues #20,#21,#23,#37,#38,#40,#41

--- client/src/app/services/user-management.service.ts
+++ client/src/app/services/user-management.service.ts
@@ -1,67 +1,76 @@
-import { AuthenticationService } from './auth.service';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { AuthenticationService } from "./auth.service";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable, throwError } from "rxjs";
+import { catchError, first, map } from "rxjs/operators";
 
-import { UserModel } from '../models/cellar.model';
+import { UserModel } from "../models/cellar.model";
 
-import * as jwt_decode from 'jwt-decode';
-import { environment } from '../../environments/environment';
+import * as jwt_decode from "jwt-decode";
+import { environment } from "../../environments/environment";
 
-import * as Debugger from 'debug';
+import * as Debugger from "debug";
 
-const debug = Debugger('app:authService');
+const debug = Debugger("app:user-management.service");
 
 const APIEndpoint = environment.APIEndpoint;
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class UserManagementService {
-	private currentUserSubject: BehaviorSubject<UserModel>;
-	public currentUser: Observable<UserModel>;
-	private backendURL: string = '';
+  private currentUserSubject: BehaviorSubject<UserModel>;
+  public currentUser: Observable<UserModel>;
+  private backendURL: string = "";
 
-	constructor(private http: HttpClient, private authenticationService: AuthenticationService) {
-		if (environment.production) this.backendURL = window.location.origin;
-		else this.backendURL = 'http://localhost:5001'; // for dev purposes
-	}
+  constructor(
+    private http: HttpClient,
+    private authenticationService: AuthenticationService
+  ) {
+    if (environment.production) this.backendURL = window.location.origin;
+    else this.backendURL = "http://localhost:5001"; // for dev purposes
+  }
 
-	updateUserData(user: UserModel) {
-		let url = this.backendURL + '/api/updateUserData';
-		return this.http.post<any>(url, user).pipe(
-			map((result) => {
-				if (result.user) {
-					// adding token resulting from previous login to update user data
-					let userTemp = JSON.parse(localStorage.getItem('currentUser'));
-					result.user.token = userTemp.token;
-					localStorage.setItem('currentUser', JSON.stringify(result.user));
-					this.authenticationService.getCurrentUserSubject().next(result.user);
-				}
-				return result;
-			})
-		);
-	}
+  updateUserData(user: UserModel) {
+    let url = this.backendURL + "/api/updateUserData";
+    return this.http.post<any>(url, user).pipe(
+      map((result) => {
+        if (result.user) {
+          // adding token resulting from previous login to update user data
+          let userTemp = JSON.parse(localStorage.getItem("currentUser"));
+          result.user.token = userTemp.token;
+          localStorage.setItem("currentUser", JSON.stringify(result.user));
+          this.authenticationService.getCurrentUserSubject().next(result.user);
+        }
+        return result;
+      })
+    );
+  }
 
-	changePassword(username, oldpwd, newpwd) {
-		let url = this.backendURL + '/api/changePassword';
-		return this.http.post<any>(url, { username: username, oldPassword: oldpwd, newPassword: newpwd }).pipe(
-			map((result) => {
-				if (result.code) {
-					// adding token resulting from previous login to update user data
-					let userTemp = JSON.parse(localStorage.getItem('currentUser'));
-				}
-				return result;
-			})
-		);
-	}
+  changePassword(username, oldpwd, newpwd) {
+    let url = this.backendURL + "/api/changePassword";
+    return this.http
+      .post<any>(url, {
+        username: username,
+        oldPassword: oldpwd,
+        newPassword: newpwd,
+      })
+      .pipe(
+        map((result) => {
+          if (result.code) {
+            // adding token resulting from previous login to update user data
+            let userTemp = JSON.parse(localStorage.getItem("currentUser"));
+          }
+          return result;
+        })
+      );
+  }
 
-	processSignupRequest(user: UserModel) {
-		let url = this.backendURL + '/api/processSignupRequest';
-		return this.http.post<any>(url, user);
-	}
+  processSignupRequest(user: UserModel) {
+    let url = this.backendURL + "/api/processSignupRequest";
+    return this.http.post<any>(url, user);
+  }
 
-	resetPassword(username) {
-		let url = this.backendURL + '/api/resetPassword';
-		return this.http.post<any>(url, { username: username });
-	}
+  resetPassword(username): Observable<any> {
+    let url = this.backendURL + "/api/resetPassword";
+    return this.http.post<any>(url, { username: username });
+  }
 }
--- client/src/app/user-management/resetpwd/resetpwd.component.ts
+++ client/src/app/user-management/resetpwd/resetpwd.component.ts
@@ -1,71 +1,80 @@
-import { TranslateService } from '@ngx-translate/core';
-import { AuthenticationService } from './../../services/auth.service';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
-import { ToastController } from '@ionic/angular';
-import { UserManagementService } from '../../services/user-management.service';
+import { TranslateService } from "@ngx-translate/core";
+import { AuthenticationService } from "./../../services/auth.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
+import { ToastController } from "@ionic/angular";
+import { UserManagementService } from "../../services/user-management.service";
 
-import * as Debugger from 'debug';
-const debug = Debugger('app:profile');
+import * as Debugger from "debug";
+const debug = Debugger("app:resetpwd");
 
 @Component({
-	selector: 'app-resetpwd',
-	templateUrl: './resetpwd.component.html',
-	styleUrls: [ './resetpwd.component.scss' ]
+  selector: "app-resetpwd",
+  templateUrl: "./resetpwd.component.html",
+  styleUrls: ["./resetpwd.component.scss"],
 })
 export class ResetpwdComponent implements OnInit {
-	resetpwdForm: FormGroup;
-	loading = false;
-	submitted = false;
+  resetpwdForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-	constructor(
-		private formBuilder: FormBuilder,
-		private router: Router,
-		private authenticationService: AuthenticationService,
-		private userManagementService: UserManagementService,
-		private toastCtrl: ToastController,
-		private translate: TranslateService
-	) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userManagementService: UserManagementService,
+    private toastCtrl: ToastController,
+    private translate: TranslateService
+  ) {}
 
-	ngOnInit() {
-		this.resetpwdForm = this.formBuilder.group({ username: [ '', Validators.required ] });
-	}
+  ngOnInit() {
+    this.resetpwdForm = this.formBuilder.group({
+      username: ["", Validators.required],
+    });
+  }
 
-	// convenience getter for easy access to form fields
-	get f() {
-		return this.resetpwdForm.controls;
-	}
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.resetpwdForm.controls;
+  }
 
-	onSubmit() {
-		this.submitted = true;
+  onSubmit() {
+    this.submitted = true;
 
-		// stop here if form is invalid
-		if (this.resetpwdForm.invalid) {
-			return;
-		}
+    // stop here if form is invalid
+    if (this.resetpwdForm.invalid) {
+      return;
+    }
 
-		this.loading = true;
-		this.userManagementService.resetPassword(this.resetpwdForm.get('username').value).pipe(first()).subscribe(
-			async (data) => {
-				const toast = await this.toastCtrl.create({
-					color: 'success',
-					message: this.translate.instant('backend.' + data.translateKey),
-					duration: 3000
-				});
-				toast.present();
-				this.router.navigate([ '/home' ]);
-			},
-			async (error) => {
-				const toast = await this.toastCtrl.create({
-					color: 'danger',
-					message: error.error.code ? this.translate.instant('backend.' + error.error.code) : error.message,
-					duration: 3000
-				});
-				toast.present();
-				this.loading = false;
-			}
-		);
-	}
+    this.loading = true;
+    this.userManagementService
+      .resetPassword(this.resetpwdForm.get("username").value)
+      .pipe(first())
+      .subscribe(
+        async (data) => {
+          debug("reset password ok");
+          const toast = await this.toastCtrl.create({
+            color: "success",
+            message: this.translate.instant("backend." + data.translateKey),
+            duration: 3000,
+          });
+          toast.present();
+          this.router.navigate(["/home"]);
+        },
+        async (error) => {
+          debug("reset password ko");
+          const toast = await this.toastCtrl.create({
+            color: "danger",
+            message: error.error.code
+              ? this.translate.instant("backend." + error.error.code)
+              : error.message,
+            duration: 3000,
+          });
+          toast.present();
+          this.loading = false;
+        }
+      );
+  }
 }
