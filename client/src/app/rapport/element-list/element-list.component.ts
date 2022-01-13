29659dd3fb40870a9c78fbf0513a2af20e091ed0
Destrais <philippe.destrais@gmail.com>
2022-01-13 17:39:50 +0100

    Changes to use ionic6

--- client/src/app/rapport/element-list/element-list.component.ts
+++ client/src/app/rapport/element-list/element-list.component.ts
@@ -1,102 +1,124 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-	selector: 'app-element-list',
-	templateUrl: './element-list.component.html',
-	styleUrls: [ './element-list.component.scss' ]
+  selector: "app-element-list",
+  templateUrl: "./element-list.component.html",
+  styleUrls: ["./element-list.component.scss"],
 })
 export class ElementListComponent implements OnInit {
-	@Input() elementList: Array<any>;
-	@Input() typeView: string;
-	@Input() elementType: string;
-	@Input() preSelectedType: string;
-	@Input() preSelectedYear: string;
-	@Input() preSelectedOrigine: string;
+  @Input() elementList: Array<any>;
+  @Input() typeView: string;
+  @Input() elementType: string;
+  @Input() preSelectedType: string;
+  @Input() preSelectedYear: string;
+  @Input() preSelectedOrigine: string;
 
-	constructor(private router: Router) {}
+  constructor(private router: Router) {}
 
-	ngOnInit() {
-		console.log('elementList : ' + this.elementList);
-	}
+  ngOnInit() {
+    console.log("elementList : " + JSON.stringify(this.elementList));
+  }
 
-	selectItem(key, value) {
-		switch (this.elementType) {
-			case 'type':
-				sessionStorage.clear();
-				if (this.typeView == 'toy') {
-					let bcType = [ { key: key, value: value, backUrlTree: [ '/rapport/bytoy/types' ] } ];
-					sessionStorage.setItem('breadcrumb', JSON.stringify(bcType));
+  selectItem(key, value) {
+    switch (this.elementType) {
+      case "type":
+        sessionStorage.clear();
+        if (this.typeView == "toy") {
+          let bcType = [
+            { key: key, value: value, backUrlTree: ["/rapport/bytoy/types"] },
+          ];
+          sessionStorage.setItem("breadcrumb", JSON.stringify(bcType));
 
-					this.router.navigate([ '/rapport/type', key, 'origines' ]);
-				} else {
-					let bcType = [ { key: key, value: value, backUrlTree: [ '/rapport/bytyo/types' ] } ];
-					sessionStorage.setItem('breadcrumb', JSON.stringify(bcType));
-					this.router.navigate([ '/rapport/type', key, 'years' ]);
-				}
-				break;
-			case 'year':
-				let bcYear = JSON.parse(sessionStorage.getItem('breadcrumb'));
-				if (this.typeView == 'toy') {
-					bcYear.push({
-						key: key,
-						value: value,
-						backUrlTree: [
-							'/rapport/type',
-							this.preSelectedType,
-							'origine',
-							this.preSelectedOrigine,
-							'years'
-						]
-					});
-					sessionStorage.setItem('breadcrumb', JSON.stringify(bcYear));
-					this.router.navigate([
-						'/rapport/wines/type',
-						this.preSelectedType,
-						'origine',
-						this.preSelectedOrigine,
-						'year',
-						key
-					]);
-				} else {
-					bcYear.push({
-						key: key,
-						value: value,
-						backUrlTree: [ '/rapport', 'type', this.preSelectedType, 'years' ]
-					});
-					sessionStorage.setItem('breadcrumb', JSON.stringify(bcYear));
-					this.router.navigate([ '/rapport/type/', this.preSelectedType, 'year', key, 'origines' ]);
-				}
-				break;
-			case 'origine':
-				let bcOrigine = JSON.parse(sessionStorage.getItem('breadcrumb'));
+          this.router.navigate(["/rapport/type", key, "origines"]);
+        } else {
+          let bcType = [
+            { key: key, value: value, backUrlTree: ["/rapport/bytyo/types"] },
+          ];
+          sessionStorage.setItem("breadcrumb", JSON.stringify(bcType));
+          this.router.navigate(["/rapport/type", key, "years"]);
+        }
+        break;
+      case "year":
+        let bcYear = JSON.parse(sessionStorage.getItem("breadcrumb"));
+        if (this.typeView == "toy") {
+          bcYear.push({
+            key: key,
+            value: value,
+            backUrlTree: [
+              "/rapport/type",
+              this.preSelectedType,
+              "origine",
+              this.preSelectedOrigine,
+              "years",
+            ],
+          });
+          sessionStorage.setItem("breadcrumb", JSON.stringify(bcYear));
+          this.router.navigate([
+            "/rapport/wines/type",
+            this.preSelectedType,
+            "origine",
+            this.preSelectedOrigine,
+            "year",
+            key,
+          ]);
+        } else {
+          bcYear.push({
+            key: key,
+            value: value,
+            backUrlTree: ["/rapport", "type", this.preSelectedType, "years"],
+          });
+          sessionStorage.setItem("breadcrumb", JSON.stringify(bcYear));
+          this.router.navigate([
+            "/rapport/type/",
+            this.preSelectedType,
+            "year",
+            key,
+            "origines",
+          ]);
+        }
+        break;
+      case "origine":
+        let bcOrigine = JSON.parse(sessionStorage.getItem("breadcrumb"));
 
-				if (this.typeView == 'toy') {
-					bcOrigine.push({
-						key: key,
-						value: value,
-						backUrlTree: [ '/rapport', 'type', this.preSelectedType, 'origines' ]
-					});
-					sessionStorage.setItem('breadcrumb', JSON.stringify(bcOrigine));
-					this.router.navigate([ '/rapport/type', this.preSelectedType, 'origine', key, 'years' ]);
-				} else {
-					bcOrigine.push({
-						key: key,
-						value: value,
-						backUrlTree: [ '/rapport/type', this.preSelectedType, 'year', this.preSelectedYear, 'origines' ]
-					});
-					sessionStorage.setItem('breadcrumb', JSON.stringify(bcOrigine));
-					this.router.navigate([
-						'/rapport/wines/type',
-						this.preSelectedType,
-						'year',
-						this.preSelectedYear,
-						'origine',
-						key
-					]);
-				}
+        if (this.typeView == "toy") {
+          bcOrigine.push({
+            key: key,
+            value: value,
+            backUrlTree: ["/rapport", "type", this.preSelectedType, "origines"],
+          });
+          sessionStorage.setItem("breadcrumb", JSON.stringify(bcOrigine));
+          this.router.navigate([
+            "/rapport/type",
+            this.preSelectedType,
+            "origine",
+            key,
+            "years",
+          ]);
+        } else {
+          bcOrigine.push({
+            key: key,
+            value: value,
+            backUrlTree: [
+              "/rapport/type",
+              this.preSelectedType,
+              "year",
+              this.preSelectedYear,
+              "origines",
+            ],
+          });
+          sessionStorage.setItem("breadcrumb", JSON.stringify(bcOrigine));
+          this.router.navigate([
+            "/rapport/wines/type",
+            this.preSelectedType,
+            "year",
+            this.preSelectedYear,
+            "origine",
+            key,
+          ]);
+        }
 
-				break;
-		}
-	}
+        break;
+    }
+  }
 }
--- client/src/app/rapport/origines/origines.component.ts
+++ client/src/app/rapport/origines/origines.component.ts
@@ -1,44 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { PouchdbService } from '../../services/pouchdb.service';
-import { ActivatedRoute } from '@angular/router';
-import { VinModel } from '../../models/cellar.model';
+import { Component, OnInit } from "@angular/core";
+import { PouchdbService } from "../../services/pouchdb.service";
+import { ActivatedRoute } from "@angular/router";
+import { VinModel } from "../../models/cellar.model";
 
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
-import * as Debugger from 'debug';
-const debug = Debugger('app:rapport:years');
+import * as Debugger from "debug";
+const debug = Debugger("app:rapport:years");
 
 @Component({
-	selector: 'app-origines',
-	templateUrl: './origines.component.html',
-	styleUrls: [ './origines.component.scss' ]
+  selector: "app-origines",
+  templateUrl: "./origines.component.html",
+  styleUrls: ["./origines.component.scss"],
 })
 export class OriginesComponent implements OnInit {
-	public typeView: string;
-	private wines: Array<VinModel>;
-	public elementList: Array<any>;
-	private elementListType: string;
-	public type: string;
-	public year: string;
-	public breadcrumb: Array<any>;
+  public typeView: string;
+  private wines: Array<VinModel>;
+  public elementList: Array<any>;
+  private elementListType: string;
+  public type: string;
+  public year: string;
+  public breadcrumb: Array<any>;
 
-	constructor(private route: ActivatedRoute, private pouchdbService: PouchdbService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private pouchdbService: PouchdbService
+  ) {}
 
-	ngOnInit() {
-		this.route.data.subscribe((data) => {
-			this.typeView = data.typeView;
-		});
-		this.type = this.route.snapshot.paramMap.get('type');
-		this.year = this.route.snapshot.paramMap.get('year');
-		// fectch breadcrumb
-		this.breadcrumb = JSON.parse(sessionStorage.getItem('breadcrumb'));
+  ngOnInit() {
+    this.route.data.subscribe((data) => {
+      this.typeView = data.typeView;
+    });
+    this.type = this.route.snapshot.paramMap.get("type");
+    this.year = this.route.snapshot.paramMap.get("year");
+    // fectch breadcrumb
+    this.breadcrumb = JSON.parse(sessionStorage.getItem("breadcrumb"));
 
-		this.pouchdbService.getDocsOfType('vin').then((docs) => {
-			this.wines = docs;
-			this.elementListType = 'origine';
-			if (this.year) {
-				// we have the type and the year
-				this.elementList = d3
+    this.pouchdbService.getDocsOfType("vin").then((docs) => {
+      this.wines = docs;
+      this.wines.map((v) => {
+        v.origine.groupVal = v.origine.pays + " - " + v.origine.region;
+      });
+
+      this.elementListType = "origine";
+      if (this.year) {
+        // we have the type and the year
+        this.elementList = d3
+          .rollups(
+            this.wines.filter(
+              (w) =>
+                w.nbreBouteillesReste != 0 &&
+                w.type.nom == this.type &&
+                w.annee == this.year
+            ),
+            (v) => d3.sum(v, (d) => d.nbreBouteillesReste),
+            (d) => d.origine.groupVal
+            /* (d) => d.origine.pays + " - " + d.origine.region */
+          )
+          .map((el) => {
+            return { key: el[0], value: el[1] };
+          });
+
+        /* 				this.elementList = d3
 					.nest()
 					.key(function(d: any) {
 						return d.origine.pays + ' - ' + d.origine.region;
@@ -53,10 +76,27 @@ export class OriginesComponent implements OnInit {
 							(w) => w.nbreBouteillesReste != 0 && w.type.nom == this.type && w.annee == this.year
 						)
 					);
-				debug('elementList (type & year selected): ' + JSON.stringify(this.elementList));
-			} else {
-				// we only have the type
-				this.elementList = d3
+ */
+        debug(
+          "elementList (type & year selected): " +
+            JSON.stringify(this.elementList)
+        );
+      } else {
+        // we only have the type
+        this.elementList = d3
+          .rollups(
+            this.wines.filter(
+              (w) => w.nbreBouteillesReste != 0 && w.type.nom == this.type
+            ),
+            (v) => d3.sum(v, (d) => d.nbreBouteillesReste),
+            (d) => d.origine.groupVal
+            /* (d) => d.origine.pays + " - " + d.origine.region */
+          )
+          .map((el) => {
+            return { key: el[0], value: el[1] };
+          });
+
+        /* 				  this.elementList = d3
 					.nest()
 					.key(function(d: any) {
 						return d.origine.pays + ' - ' + d.origine.region;
@@ -67,11 +107,14 @@ export class OriginesComponent implements OnInit {
 						});
 					})
 					.entries(this.wines.filter((w) => w.nbreBouteillesReste != 0 && w.type.nom == this.type));
-				debug('elementList (type selected): ' + JSON.stringify(this.elementList));
-			}
-			this.elementList.sort((a, b) => {
-				return a.key < b.key ? -1 : 1;
-			});
-		});
-	}
+ */
+        debug(
+          "elementList (type selected): " + JSON.stringify(this.elementList)
+        );
+      }
+      this.elementList.sort((a, b) => {
+        return a.key < b.key ? -1 : 1;
+      });
+    });
+  }
 }
